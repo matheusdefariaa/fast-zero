72300ef353f38a3aecabdc17145eba6d092de047
matheus <matheusdefaria@proton.me>
2025-10-04 11:54:46 -0300

    Criação do schemas e novo teste

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,23 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.get('/ola', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def resposta_ola():
+    return """
+    <html>
+    <head><title>FastAPI</title></head>
+    <body><h1>Olá Mundo</h1></body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,17 @@ def test_read_root_dev_retorna_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_ola():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    text = """
+    <html>
+    <head><title>FastAPI</title></head>
+    <body><h1>Olá Mundo</h1></body>
+    </html>
+    """
+    assert response.text == text
