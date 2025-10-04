c2ab7326a901643b9543e2c94e227e7d92968e0c
matheus <matheusdefaria@proton.me>
2025-10-04 14:40:16 -0300

    retirando rota e teste

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,23 +1,18 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+# from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI()
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-def read_root():
+def read_root() -> dict[str, str]:
     return {'message': 'Hello World'}
 
 
-@app.get('/ola', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-def resposta_ola():
-    return """
-    <html>
-    <head><title>FastAPI</title></head>
-    <body><h1>Olá Mundo</h1></body>
-    </html>
-    """
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema) -> UserPublic:
+    return user
