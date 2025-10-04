24a250242dd9366fa0710ed4af49741dca3b9306
matheus <matheusdefaria@proton.me>
2025-10-04 16:30:53 -0300

    adicionando verificação de id

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 # from fastapi.responses import HTMLResponse
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
@@ -31,6 +31,8 @@ def read_users():
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
 def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='NOT FOUND')
     user_with_id = UserDB(**user.model_dump(), id=user_id)
     database[user_id - 1] = user_with_id
     return user_with_id
