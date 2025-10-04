f6890d4b96b1c21a5940b4150af28ce234d831df
matheus <matheusdefaria@proton.me>
2025-10-04 16:21:56 -0300

    criação de teste para update

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,3 +39,21 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        'users/1',
+        json={
+            'username': 'teste2',
+            'email': 'teste@test.com',
+            'id': 1,
+            'password': '123',
+        },
+    )
+
+    assert response.json() == {
+        'username': 'teste2',
+        'email': 'teste@test.com',
+        'id': 1,
+    }
